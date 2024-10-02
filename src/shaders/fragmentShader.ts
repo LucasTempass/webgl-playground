234e41542baffb4d4e53073b90a61b49634dbe69
Lucas Cerveira <lucas.tempass@hotmail.com>
2024-10-02 20:29:17 -0300

    Ajusta shaders

--- src/shaders/fragmentShader.ts
+++ src/shaders/fragmentShader.ts
@@ -1,10 +1,32 @@
 export const fragmentShaderContent = `#version 300 es
 precision highp float;
 
-in vec4 color;
+in vec3 color;
+in vec3 scaled_normal;
+in vec3 position;
+
+// props. da superfície
+uniform float ka, kd, ks, q;
+uniform vec3 light_position, light_color;
+uniform vec3 camera_pos;
 
 out vec4 fragment_color;
 
 void main() {
-    fragment_color = color;
+    vec3 ambient = ka * light_color;
+
+    vec3 N = normalize(scaled_normal);
+    vec3 L = normalize(light_position - position);
+    float diff = max(dot(N,L),0.0);
+    vec3 diffuse = kd * diff * light_color;
+
+    vec3 R = normalize(reflect(-L, N));
+    vec3 V = normalize(camera_pos - position);
+    float spec = max(dot(R,V),0.0);
+   
+    vec3 specular = ks * pow(spec, q) * light_color;
+
+    vec3 result = (ambient + diffuse) * color + specular;
+
+    fragment_color = vec4(result, 1.0);
 }`;
--- src/shaders/vertexShader.ts
+++ src/shaders/vertexShader.ts
@@ -1,12 +1,17 @@
 export const vertexShaderContent = `#version 300 es
-in vec3 position;
+in vec3 base_position;
 in vec3 base_color;
 
 uniform mat4 model;
 
-out vec4 color;
+out vec3 color;
+out vec3 scaled_normal;
+out vec3 position;
 
 void main() {
-    gl_Position = model * vec4(position, 1.0);
-    color = vec4(base_color, 1.0);
+    gl_Position = model * vec4(base_position, 1.0);
+    color = base_color;
+    // reduz para vec3 para facilitar cálculos no fragment shader
+    scaled_normal = vec3(gl_Position);
+    position = vec3(gl_Position);
 }`;
